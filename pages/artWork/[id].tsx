415526fa75dd245d497cfe437d10d47b160fc4c8
timjames <timjames@pingidentity.com>
2022-01-05 16:46:32 +0000

    adding form

--- pages/artWork/[id].tsx
+++ pages/artWork/[id].tsx
@@ -1,6 +1,7 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/artWork'
 import Link from 'next/link';
+import React, { useState } from 'react';
 
 export async function getStaticPaths() {
     const paths = getAllPostIds();
@@ -38,6 +39,29 @@ export async function getStaticProps({ params }: IParams) {
 }
 
 export default function Post({ postData }: IPostData) {
+
+    const [ name, setName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ message, setMessage ] = useState('');
+    const [ messageSending, setMessageSending ] = useState(false);
+    const [ messageSent, setMessageSent ] = useState(false);
+
+    const sendEnquiry = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+        setMessageSending(true);
+
+        const res = await fetch(`https://us-central1-art-by-eva.cloudfunctions.net/addMessage?text=${message}&name=${name}&email=${email}`);
+
+        if (res.ok) {
+            setMessageSent(true);
+        }
+
+        //const result = await res.json();
+
+        setMessageSending(false);
+    };
+
     return (
         <Layout>
             <div className="container">
@@ -62,6 +86,41 @@ export default function Post({ postData }: IPostData) {
                             Price: &pound;{postData.price}
                             <br />
                             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+
+                            <header>
+                                <h6>Enquire</h6>
+                            </header>
+                            {
+                                !messageSent
+                                ? (
+                                    <>
+                                        <p>Please complete the following fields to send an enquiry.</p>
+                                        <form onSubmit={sendEnquiry}>
+                                            <div className="mb-3">
+                                                <label className="form-label">Name</label>
+                                                <input type="text" className="form-control" value={name} onChange={(e) => {setName(e.currentTarget.value)}} />
+                                            </div>
+                                            <div className="mb-3">
+                                                <label className="form-label">Email</label>
+                                                <input type="text" className="form-control" value={email} onChange={(e) => {setEmail(e.currentTarget.value)}} />
+                                            </div>
+                                            <div className="mb-3">
+                                                <label className="form-label">Message</label>
+                                                <textarea className="form-control" value={message} onChange={(e) => {setMessage(e.currentTarget.value)}}>
+                                                </textarea>
+                                            </div>
+                                            <button type="submit" className="btn btn-primary" disabled={messageSending}>
+                                                {
+                                                    messageSending ? 'Processing' : 'Enquire Now'
+                                                }
+                                            </button>
+                                        </form>
+                                    </>
+                                )
+                                : (
+                                    <p>Your Enquiry has been submitted.</p>
+                                )
+                            }
                         </div>
                         <div className="col-12 col-md-6">
                             {
